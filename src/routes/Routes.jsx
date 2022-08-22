0c39ec1046242b798cd7a18325660767d9d088ec
Roberto <robertog.adm@gmail.com>
2022-08-22 18:09:09 -0300

    Req 16 ate Req 18 (Sem teste)

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -6,6 +6,10 @@ import Drinks from '../pages/Drinks/Drinks';
 import Profile from '../pages/Profile/Profile';
 import DoneRecipes from '../pages/DoneRecipes/DoneRecipes';
 import FavoriteRecipes from '../pages/FavoriteRecipes/FavoriteRecipes';
+import FoodsId from '../pages/FoodsId/FoodsId';
+import DrinksId from '../pages/DrinksId/DrinksId';
+import FoodsInProgress from '../pages/FoodsInProgress/FoodsInProgress';
+import DrinksInProgress from '../pages/DrinksInProgress/DrinksInProgress';
 
 const Routes = () => (
   <BrowserRouter>
@@ -13,10 +17,10 @@ const Routes = () => (
       <Route exact path="/" component={ Login } />
       <Route exact path="/foods" component={ Foods } />
       <Route exact path="/drinks" component={ Drinks } />
-      <Route exact path="foods/:id" component={ Foods } />
-      <Route exact path="drinks/:id" component={ Drinks } />
-      <Route exact path="/foods/:id/in-progress" component={ Foods } />
-      <Route exact path="/drinks/:id/in-progress" component={ Drinks } />
+      <Route exact path="foods/:id" component={ FoodsId } />
+      <Route exact path="drinks/:id" component={ DrinksId } />
+      <Route exact path="/foods/:id/in-progress" component={ FoodsInProgress } />
+      <Route exact path="/drinks/:id/in-progress" component={ DrinksInProgress } />
       <Route exact path="/profile" component={ Profile } />
       <Route exact path="/done-recipes" component={ DoneRecipes } />
       <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
